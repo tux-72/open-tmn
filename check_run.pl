2ba05f64d2f405d5f92114973a39b4a891926af1
ban <ban@e14fafcf-fe10-7c38-c327-7632231bdbb8>
2010-04-13 06:10:17 +0000

    fix cycle check run script

--- check_run.pl
+++ check_run.pl
@@ -8,8 +8,7 @@ $LOGDIR		= "/var/log/swctl";
 $run_user	= "billsync";
 
 my @processes = ( 	"checkterm",
-	    		"checkport",
-#	    		"checklink",
+	    		"checkjobs",
 		);
 $date = `date +%F.%H:%M:%S`;
 $date =~ s/\n//g;
@@ -17,17 +16,8 @@ $date =~ s/\n//g;
 if ( $ARGV[0] eq "real" ) {
 
     foreach $PROC ( @processes ) { 
-	my $proc_found = system ('( /bin/ps ax | /usr/bin/grep '.$exec_file.' | /usr/bin/grep '.$PROC.' | /usr/bin/grep -v grep ) > /dev/null' );
-	#print STDERR "FOUND = $proc_found\n";
-	if ( $proc_found ) {
-	    print STDERR "$date\trun new process $PROC\n";
-	    #$SH_CMD = '/usr/bin/su '.$run_user.' -c "( '.$Bin.'/bin/'.$exec_file.' '.$PROC.' '.$LOGDIR.'/'.$PROC.'.log ) &"';
 	    $SH_CMD = '/usr/bin/su '.$run_user.' -c "( '.$Bin.'/bin/'.$exec_file.' '.$PROC.' >> '.$LOGDIR.'/'.$PROC.'-stderr.log 2>&1 ) &"';
-	    #print STDERR "$SH_CMD\n";
 	    system ( $SH_CMD );
-	#} else {
-	#    print STDERR "$date\tprocess $PROC already running\n";
-	}
     }
 
 } else {
