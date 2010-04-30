9180c88309d025325d77727f3cf74525c43e3b6b
ban <ban@e14fafcf-fe10-7c38-c327-7632231bdbb8>
2010-04-30 08:09:38 +0000

    change dispatcher sql update command again

--- lib/SWFunc.pm
+++ lib/SWFunc.pm
@@ -480,7 +480,7 @@ sub SW_AP_get {
 			## Temporary inserting data
 			if ( $AP{'trust'} and ( not $AP{'communal'}) and $fparm->{'link_type'} == $link_type{'pppoe'} and $fparm->{'ip_addr'} =~ /^10\./ ) {
 			    $Query = "INSERT INTO head_link SET port_id=".$AP{'id'}.", status=1, static_ip=0, ";
-			    $Q_upd = " head_id=3, vlan_id=".$AP{'VLAN'}.", login='".$fparm->{'login'}."', hw_mac='".$fparm->{'mac'}."', pppoe_up=1, vlan_id=".$AP{'VLAN'}.
+			    $Q_upd = " head_id=3, vlan_id=".$AP{'VLAN'}.", login='".$fparm->{'login'}."', hw_mac='".$fparm->{'mac'}."', pppoe_up=1".
 			    ", inet_shape=".$fparm->{'inet_rate'}.", inet_priority=".$fparm->{'inet_priority'}.", stamp=NULL, ip_subnet='".$fparm->{'ip_addr'}."'";
 			    $Query .= $Q_upd." ON DUPLICATE KEY UPDATE ".$Q_upd;
 			$dbm->do("$Query");
