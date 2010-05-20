c394951413580c65a05d319e685b6630d7eb2bb8
ban <ban@e14fafcf-fe10-7c38-c327-7632231bdbb8>
2010-05-20 13:23:56 +0000

    tune dhcpd2db.pl for more dhcp-client vendors

--- dhcp2radius/dhcpd2db.pl
+++ dhcp2radius/dhcpd2db.pl
@@ -122,7 +122,7 @@ sub post_auth {
  			return RLM_MODULE_NOTFOUND;
 		    }
 		    # Выделить IP
-		    my $Q_Discover_start  = "SELECT a.login, a.ip, a.start_lease, a.end_lease, p.mask, p.gw, p.static_ip, p.dhcp_lease FROM dhcp_addr a, dhcp_pools p ".
+		    my $Q_Discover_start  = "SELECT a.login, a.ip, a.start_lease, a.end_lease, p.mask, p.gw, p.dhcp_lease FROM dhcp_addr a, dhcp_pools p ".
 		    " WHERE p.head_id=".$ref_port->{'head_id'}." and p.pool_id=a.pool_id";
 
 		    my $Q_Discover_reuse = ""; my $Q_Discover_new ='' ; my $Q_Discover_grey ='' ;
@@ -185,11 +185,13 @@ sub post_auth {
 			$RAD_REPLY{'DHCP-IP-Address-Lease-Time'} = $ref_disc->{'dhcp_lease'};
 			$RAD_REPLY{'DHCP-Your-IP-Address'}	 = $ref_disc->{'ip'};
 			$RAD_REPLY{'DHCP-Subnet-Mask'}		 = $ref_disc->{'mask'};
-			$RAD_REPLY{'DHCP-Router-Address'}	 = $ref_disc->{'gw'};
+			if ( $ref_port->{'static_ip'} || ( not $start_conf->{'DHCP_DYN_GREYIP'} )) {
+			     $RAD_REPLY{'DHCP-Router-Address'}	 = $ref_disc->{'gw'};
+			}
 			my $Q_Disc_up = "UPDATE dhcp_addr SET agent_info='".$RAD_REQUEST{'DHCP-Relay-Agent-Information'}."', login='".$ref_port->{'login'}."'".
 			", port_id=".$ref_port->{'port_id'}.", vlan_id=".$vlan.", hw_mac='".$RAD_REQUEST{'DHCP-Client-Hardware-Address'}."', start_lease=now() ".
 			( $AP{'new_lease'} ? ", start_use=now()" : "" ).", end_lease=ADDDATE(now(), INTERVAL ".$ref_disc->{'dhcp_lease'}." SECOND)".
-			" WHERE ip='".$ref_disc->{'ip'}."'";
+			", dhcp_vendor='".$RAD_REQUEST{'DHCP-Vendor-Class-Identifier'}."' WHERE ip='".$ref_disc->{'ip'}."'";
 			$rows_up = $dbm->do($Q_Disc_up);
 			$res = RLM_MODULE_OK if $rows_up == 1;
 		    }
@@ -219,7 +221,7 @@ sub post_auth {
 		&radiusd::radlog(1, "CLI_IP = '".$cli_addr."'") if $debug;
 		&radiusd::radlog(1, "ID_session ='".$RAD_REQUEST{'DHCP-Transaction-Id'}."'") if $debug;
 
-		my $Q_Request = "SELECT a.session, a.ip, a.port_id, a.start_lease, p.mask, p.gw, p.static_ip, p.dhcp_lease, l.login, h.term_ip".
+		my $Q_Request = "SELECT a.session, a.ip, a.port_id, a.start_lease, p.mask, p.gw, l.static_ip, p.dhcp_lease, l.login, h.term_ip".
 		" FROM dhcp_addr a, dhcp_pools p, head_link l, heads h WHERE l.head_id=h.head_id and l.login=a.login and l.hw_mac=a.hw_mac".
 		" and a.port_id=l.port_id and a.pool_id=p.pool_id and l.status=1 and l.inet_priority<=".$start_conf->{'DHCP_PRI'}." and l.communal=0".
 		" and l.dhcp_use=1 and h.dhcp_relay_ip='".$RAD_REQUEST{'DHCP-Gateway-IP-Address'}."' and a.ip='".$cli_addr."' and a.agent_info='".
@@ -232,7 +234,7 @@ sub post_auth {
 		if  ( $stm_req->rows == 1 ) {
 		    while (my $ref_req = $stm_req->fetchrow_hashref()) {
 
-			if ( $ref_req->{'session'} ne $RAD_REQUEST{'DHCP-Transaction-Id'} ) {
+			if ( ( not defined ($ref_req->{'session'}) ) || ($ref_req->{'session'} ne $RAD_REQUEST{'DHCP-Transaction-Id'}) ) {
 			    SW_AP_fix( AP_INFO => \%AP, NAS_IP => $ref_req->{'term_ip'}, LOGIN => $ref_req->{'login'} , VLAN => $AP{'VLAN'}, HW_MAC => $AP{'MAC'} );
 			    if ( $AP{'id'} != $ref_req->{'port_id'} ) {
 				$RAD_REPLY{'DHCP-Message-Type'} = 'DHCP-NAK';
@@ -245,10 +247,12 @@ sub post_auth {
 			$RAD_REPLY{'DHCP-IP-Address-Lease-Time'} = $ref_req->{'dhcp_lease'};
 			$RAD_REPLY{'DHCP-Your-IP-Address'}	 = $ref_req->{'ip'};
 			$RAD_REPLY{'DHCP-Subnet-Mask'}		 = $ref_req->{'mask'};
-			$RAD_REPLY{'DHCP-Router-Address'}	 = $ref_req->{'gw'};
+			if ( $ref_req->{'static_ip'} || ( not $start_conf->{'DHCP_DYN_GREYIP'} ) ) {
+			    $RAD_REPLY{'DHCP-Router-Address'}    = $ref_req->{'gw'};
+			}
 
 			my $Q_Request_up =  "UPDATE dhcp_addr SET end_lease=ADDDATE(now(), INTERVAL ".$ref_req->{'dhcp_lease'}.
-			" SECOND ), session='".$RAD_REQUEST{'DHCP-Transaction-Id'}."'".
+			" SECOND ), session='".$RAD_REQUEST{'DHCP-Transaction-Id'}."', dhcp_vendor='".$RAD_REQUEST{'DHCP-Vendor-Class-Identifier'}."'".
 			" WHERE agent_info='".$RAD_REQUEST{'DHCP-Relay-Agent-Information'}.
 			"' and hw_mac='".$RAD_REQUEST{'DHCP-Client-Hardware-Address'}."'".
 			" and ip='".$cli_addr."'";
